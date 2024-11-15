3c431e0fbaed77736751ec953adba2aaa0bdeae3
Shrey K <srkkadia@gmail.com>
2024-11-15 17:02:34 +0530

    init

---
+++ FirstApp/src/main/java/basics/FirstServlet.java
@@ -0,0 +1,21 @@
+package basics;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		
+		out.println("<h1>Hello, World!</h1>");
+	}
+
+}
